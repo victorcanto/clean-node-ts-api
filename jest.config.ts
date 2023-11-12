d0df47ff07ffb91f32bbbd4a2f102908a99a05a1
Victor Canto <iamvictorcanto@gmail.com>
2023-11-12 19:51:50 -0300

    chore: add jest config and eslint

--- jest.config.ts
+++ jest.config.ts
@@ -8,10 +8,9 @@ import type { Config } from 'jest'
 const config: Config = {
   roots: ['<rootDir>/src'],
   clearMocks: true,
-  collectCoverage: true,
+  // collectCoverage: true,
   collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
-  coverageProvider: 'v8',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
