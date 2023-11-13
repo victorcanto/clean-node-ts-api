15cf1e6a42072feb38567fcff4a5848f9efb76b4
Victor Canto <iamvictorcanto@gmail.com>
2023-11-13 18:57:09 -0300

    feat: add Data layer

---
+++ src/data/usecases/add-account/db-add-account-protocols.ts
@@ -0,0 +1,4 @@
+export type { AddAccount, AddAccountModel } from '../../../domain/usecases/add-account.usecase'
+export type { AccountModel } from '../../../domain/models/account.model'
+export type { Encrypter } from '../../protocols/encrypter'
+export type { AddAccountRepository } from '../../protocols/add-account-repository'
---
+++ src/data/usecases/add-account/db-add-account.usecase.ts
@@ -0,0 +1,13 @@
+import { type AccountModel, type AddAccount, type AddAccountModel, type Encrypter, type AddAccountRepository } from './db-add-account-protocols'
+
+export class DbAddAccount implements AddAccount {
+  constructor (
+    private readonly encrypter: Encrypter,
+    private readonly addAccountRepository: AddAccountRepository) {}
+
+  async add (accountData: AddAccountModel): Promise<AccountModel> {
+    const hashedPassword = await this.encrypter.encrypt(accountData.password)
+    const account = await this.addAccountRepository.add({ ...accountData, password: hashedPassword })
+    return account
+  }
+}
