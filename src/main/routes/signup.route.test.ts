59ce6a0e5d37f1fb2e0bc1895d0685baec904214
Victor Canto <iamvictorcanto@gmail.com>
2023-11-15 01:49:22 -0300

    feat: add SignUp route and test

--- src/main/routes/signup.route.test.ts
+++ src/main/routes/signup.route.test.ts
@@ -1,7 +1,21 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoDbHelper } from '../../infra/db/mongodb/helpers/mongodb.helper'
 
 describe('CORS Middleware', () => {
+  beforeAll(async () => {
+    await MongoDbHelper.connect(process.env.MONGO_URL as string)
+  })
+
+  afterAll(async () => {
+    await MongoDbHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoDbHelper.getCollection('accounts')
+    await accountCollection.deleteMany()
+  })
+
   test('Should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
