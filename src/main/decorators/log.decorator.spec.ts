ff92e4026f347c0170cdc941ab0d7d917a5c0e85
Victor Canto <iamvictorcanto@gmail.com>
2023-11-15 17:01:57 -0300

    feat: add log decorator and test

---
+++ src/main/decorators/log.decorator.spec.ts
@@ -0,0 +1,75 @@
+import { type HttpRequest, type HttpResponse, type Controller } from '../../presentation/protocols'
+import { LogControllerDecorator } from './log.decorator'
+
+interface SutTypes {
+  sut: Controller
+  controllerStub: Controller
+}
+
+const makeController = (): Controller => {
+  class ControllerStub implements Controller {
+    async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
+      const httpResponse: HttpResponse = {
+        body: {
+          name: 'any_name',
+          email: 'any_email',
+          password: 'any_password',
+          passwordConfirmation: 'any_password'
+        },
+        statusCode: 200
+      }
+      return await new Promise((resolve, reject) => { resolve(httpResponse) })
+    }
+  }
+  return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+  const controllerStub = makeController()
+  const sut = new LogControllerDecorator(controllerStub)
+  return {
+    sut,
+    controllerStub
+  }
+}
+
+describe('LogController Decorator', () => {
+  test('Should call controller handle', async () => {
+    const { sut, controllerStub } = makeSut()
+    const handleSpy = jest.spyOn(controllerStub, 'handle')
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    await sut.handle(httpRequest)
+    expect(handleSpy).toHaveBeenCalledWith(httpRequest)
+  })
+
+  test('Should return the same result of the controller', async () => {
+    const { sut } = makeSut()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual({
+      body: {
+        name: 'any_name',
+        email: 'any_email',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      },
+      statusCode: 200
+    })
+  })
+})
