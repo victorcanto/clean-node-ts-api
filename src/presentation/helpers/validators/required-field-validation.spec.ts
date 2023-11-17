393ec1a3224248e58fb15b5d1422bd60c79d0b9a
Victor Canto <iamvictorcanto@gmail.com>
2023-11-17 02:04:23 -0300

    test: ensure RequiredFieldValidation returns a MissingParamError if validation fails

---
+++ src/presentation/helpers/validators/required-field-validation.spec.ts
@@ -0,0 +1,10 @@
+import { MissingParamError } from '../../errors'
+import { RequiredFieldValidation } from './required-field-validation'
+
+describe('RequiredField Validation', () => {
+  test('Should return MissingParamError if validation fails', async () => {
+    const sut = new RequiredFieldValidation('field')
+    const error = sut.validate({ name: 'any_name' })
+    expect(error).toEqual(new MissingParamError('field'))
+  })
+})
