a0b17af8d1cc1699e8ead13f052e76486aa18e8f
Victor Canto <iamvictorcanto@gmail.com>
2024-01-24 12:30:40 -0300

    feat: implements Decrypter on JwtAdapter

--- src/infra/criptography/jwt/jwt-adapter.ts
+++ src/infra/criptography/jwt/jwt-adapter.ts
@@ -1,10 +1,15 @@
 import jwt from 'jsonwebtoken'
 import { type Encrypter } from '../../../data/protocols/criptography/encrypter.protocol'
+import { type Decrypter } from '../../../data/protocols/criptography/decrypter.protocol'
 
-export class JwtAdapter implements Encrypter {
+export class JwtAdapter implements Encrypter, Decrypter {
   constructor (private readonly secret: string) {}
 
   async encrypt (value: string): Promise<string> {
     return jwt.sign({ id: value }, this.secret)
+  }
+
+  async decrypt (token: string): Promise<string> {
+    return jwt.verify(token, this.secret) as any
   }
 }
