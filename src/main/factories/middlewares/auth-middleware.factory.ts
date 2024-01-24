c70fac1e675025245239d4b1d66376081bb60a54
Victor Canto <iamvictorcanto@gmail.com>
2024-01-24 15:24:50 -0300

    feat: add AuthMiddleware factory

---
+++ src/main/factories/middlewares/auth-middleware.factory.ts
@@ -0,0 +1,9 @@
+import { AuthMiddleware } from '../../../presentation/middlewares/auth-middleware'
+import { type Middleware } from '../../../presentation/protocols'
+import { makeDbLoadAccountByToken } from '../usecases/account/load-account-by-token/db-load-account-by-token.factory'
+
+export const makeAuthMiddleware = (role?: string): Middleware => {
+  const loadAccountByTokenRepository = makeDbLoadAccountByToken()
+  const middleware = new AuthMiddleware(loadAccountByTokenRepository, role)
+  return middleware
+}
