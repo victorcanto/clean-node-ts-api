db9753d1373482af5c99f2f2caa3e20cc245b7ca
Victor Canto <iamvictorcanto@gmail.com>
2024-01-30 12:52:36 -0300

    fix: statusCode of forbidden

--- src/presentation/helpers/http/http.helper.ts
+++ src/presentation/helpers/http/http.helper.ts
@@ -7,7 +7,7 @@ export const badRequest = (error: Error): HttpResponse => ({
 })
 
 export const forbidden = (error: Error): HttpResponse => ({
-  statusCode: 400,
+  statusCode: 403,
   body: error
 })
 
