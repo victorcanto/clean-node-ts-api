d80ada3e600835eba0c64927ca04e370d4962c2e
Victor Canto <iamvictorcanto@gmail.com>
2024-01-30 12:50:59 -0300

    refactor: renames controller to middleware

--- src/main/adapters/express-middleware.adapter.ts
+++ src/main/adapters/express-middleware.adapter.ts
@@ -1,12 +1,12 @@
 import { type NextFunction, type Request, type Response } from 'express'
 import { type HttpRequest, type Middleware } from '../../presentation/protocols'
 
-export const adaptMiddleware = (controller: Middleware) => {
+export const adaptMiddleware = (middleware: Middleware) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     const httpRequest: HttpRequest = {
       headers: req.headers
     }
-    const httpResponse = await controller.handle(httpRequest)
+    const httpResponse = await middleware.handle(httpRequest)
     if (httpResponse.statusCode === 200) {
       Object.assign(req, httpResponse.body)
       next()
