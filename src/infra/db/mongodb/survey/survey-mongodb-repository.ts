8acf2f23e8f5d93db23388c7c288c0865602b4d5
Victor Canto <iamvictorcanto@gmail.com>
2024-05-01 00:57:39 -0300

    feat: ensure SurveyMongoDbRepository load all surveys

--- src/infra/db/mongodb/survey/survey-mongodb-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongodb-repository.ts
@@ -1,10 +1,17 @@
 import { type AddSurveyRepository } from '../../../../data/usecases/add-survey/db-add-survey-protocols'
+import { type SurveyModel, type LoadSurveysRepository } from '../../../../data/usecases/load-surveys/db-load-surveys-protocols'
 import { type AddSurveyModel } from '../../../../domain/usecases/add-survey.usecase'
 import { MongoDbHelper } from '../helpers/mongodb.helper'
 
-export class SurveyMongoDbRepository implements AddSurveyRepository {
+export class SurveyMongoDbRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoDbHelper.getCollection('surveys')
     await surveyCollection.insertOne(surveyData)
+  }
+
+  async loadAll (): Promise<SurveyModel[]> {
+    const surveyCollection = await MongoDbHelper.getCollection('surveys')
+    const surveys = await surveyCollection.find().toArray() as unknown as SurveyModel[]
+    return surveys
   }
 }
