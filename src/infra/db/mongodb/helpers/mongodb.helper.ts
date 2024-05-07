0afacd7f279484b3456cecd39bb1dffdf30a4214
Victor Canto <iamvictorcanto@gmail.com>
2024-05-07 16:53:26 -0300

    refactor: ensure MongoDbHelper has 100 of test coverage

--- src/infra/db/mongodb/helpers/mongodb.helper.ts
+++ src/infra/db/mongodb/helpers/mongodb.helper.ts
@@ -5,21 +5,13 @@ export const MongoDbHelper = {
   uri: null as unknown as string,
 
   async connect (uri: string): Promise<void> {
-    try {
-      this.uri = uri
-      this.client = await MongoClient.connect(uri)
-    } catch (error) {
-      console.error('Failed to connect to MongoDB', error)
-    }
+    this.uri = uri
+    this.client = await MongoClient.connect(uri)
   },
 
   async disconnect (): Promise<void> {
-    try {
-      await this.client.close()
-      this.client = null
-    } catch (error) {
-      console.error('Failed to disconnect to MongoDB', error)
-    }
+    await this.client.close()
+    this.client = null
   },
 
   async getCollection (name: string): Promise<Collection> {
