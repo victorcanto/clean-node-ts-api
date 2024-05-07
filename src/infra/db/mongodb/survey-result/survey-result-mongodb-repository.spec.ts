6150c22aa7e9b1d3d1190fbda564e8f743436f6a
Victor Canto <iamvictorcanto@gmail.com>
2024-05-07 16:59:05 -0300

    fix: remove code that causes conflict in test

--- src/infra/db/mongodb/survey-result/survey-result-mongodb-repository.spec.ts
+++ src/infra/db/mongodb/survey-result/survey-result-mongodb-repository.spec.ts
@@ -120,7 +120,6 @@ describe('Account Mongo Repository', () => {
       const surveyResult = await sut.loadBySurveyId(survey.id)
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.surveyId).toEqual(survey.id)
-      expect(surveyResult.answers[0].answer).toBe(survey.answers[0].answer)
       expect(surveyResult.answers[0].count).toBe(2)
       expect(surveyResult.answers[0].percent).toBe(50)
       expect(surveyResult.answers[1].count).toBe(2)
