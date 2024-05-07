fee81ad4d94579b2ed4283a35100156d5aeab4e3
Victor Canto <iamvictorcanto@gmail.com>
2024-05-07 10:09:15 -0300

    fix: change survey result to return all answers

--- src/infra/db/mongodb/helpers/query-builder.ts
+++ src/infra/db/mongodb/helpers/query-builder.ts
@@ -11,6 +11,11 @@ export class QueryBuilder {
     return this
   }
 
+  sort (data: object): QueryBuilder {
+    this.query.push({ $sort: data })
+    return this
+  }
+
   unwind (data: object): QueryBuilder {
     this.query.push({ $unwind: data })
     return this
