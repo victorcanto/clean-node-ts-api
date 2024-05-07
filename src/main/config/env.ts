15c0956133e2271f8c41d139640d2750f2f4e0a3
Victor Canto <iamvictorcanto@gmail.com>
2024-05-06 21:48:51 -0300

    feat: change api port

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -1,5 +1,5 @@
 export default {
   mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
-  port: process.env.PORT ?? 5000,
+  port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'asdf++==5YKZ'
 }
